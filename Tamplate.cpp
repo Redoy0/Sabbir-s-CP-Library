e7d84152a6bb7f6275632998cdc82067aeabc5c7
Md Sabbir Ahamed <52445722+Redoy0@users.noreply.github.com>
2023-04-11 16:07:57 +0600

    Create Tamplate.cpp

---
+++ Tamplate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define Fast ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define vi vector<int>
+#define sp setprecision
+#define forn(i,n) for(int i=0;i<n;i++)
+
+void solve()
+{
+    
+
+
+}
+int main()
+{   Fast;
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+    return 0;
+}
