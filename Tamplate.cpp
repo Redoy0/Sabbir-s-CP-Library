1dbeeea14f647138147590b8b86299538791e400
Md Sabbir Ahamed <52445722+Redoy0@users.noreply.github.com>
2023-05-03 20:02:07 +0600

    full tamplate updated

--- Tamplate.cpp
+++ Tamplate.cpp
@@ -1,23 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
 #define endl "\n"
-#define Fast ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define vi vector<int>
-#define sp setprecision
-#define forn(i,n) for(int i=0;i<n;i++)
+#define Fast ios_base :: sync_with_stdio(0);cin.tie(0);
+#define setp setprecision
+#define forn(i,j,n) for(int i=(j);i<n;i++)
 
-void solve()
+#define mod         1000000007
+#define pi          acos(-1.0)
+#define eps         1e-9
+
+#define fs          first
+#define sc          second
+#define all(x)      x.begin(),x.end()
+#define pb(a)       push_back(a)
+#define mp(a,b)     make_pair(a,b)
+#define sp          printf(" ")
+#define nl          printf("\n")
+
+#define set0(a)     memset(a,0,sizeof(a))
+#define setneg(a)   memset(a,-1,sizeof(a))
+#define setinf(a)   memset(a,126,sizeof(a))
+
+#define tc1(x)      printf("Case %d: ",x)
+#define tc2(x)      printf("Case #%d: ",x)
+#define tc3(x)      printf("Case %d:\n",x)
+#define tc4(x)      printf("Case #%d:\n",x)
+
+#define pr1(x)      cout<<x<<"\n"
+#define pr2(x,y)    cout<<x<<" "<<y<<"\n"
+#define pr3(x,y,z)  cout<<x<<" "<<y<<" "<<z<<"\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int>pii;
+typedef pair<ll, ll>pll;
+typedef pair<double, double>pdd;
+typedef vector<int>vi;
+typedef vector<ll>vll;
+typedef vector<double>vd;
+
+int solve()
 {
 
 
 
 }
 int main()
-{   Fast;
-    int t;
+{   //Fast;
+    int t=1,tc=1;
     cin>>t;
-    while(t--)
+    while(tc<=t){
+        tc1(tc);
         solve();
+
+    tc++;
+    }
+
     return 0;
 }
