80b66766ec3126a76399ea92e84eedc5820ea29b
Md Sabbir Ahamed <52445722+Redoy0@users.noreply.github.com>
2023-09-04 02:35:04 +0600

    Update InsertionSort.c

--- Algorithms/InsertionSort.c
+++ Algorithms/InsertionSort.c
@@ -1,1 +1,25 @@
+//insertion sort by Rs
+#include<stdio.h>
+int main()
+{
+    int a[]={0,5,8,1,2};
+    int n=sizeof(a)/sizeof(int),j,temp;
+    printf("Before Insertion Sorting\n");
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+    for(int i=1,j=0;i<n;i++)
+    {
+        temp=a[i];
+        for(j=i-1;j>=0 && a[j]>temp;j--)
+        {
+            a[j+1]=a[j];
+        }
+        a[j+1]=temp;
+    }
+
+printf("\nAter Using Insertion Sort");
+    printf("\n");
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+}
 
