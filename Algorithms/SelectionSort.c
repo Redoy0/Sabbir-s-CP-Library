d83d0e51f8f8a2631efa01323f048778645e3290
Md Sabbir Ahamed <52445722+Redoy0@users.noreply.github.com>
2023-09-24 13:36:23 +0600

    Update SelectionSort.c

--- Algorithms/SelectionSort.c
+++ Algorithms/SelectionSort.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a[]={12,1,3,4,7};
+    int n=sizeof(a)/sizeof(a[0]);
+
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+                min=j;
+        }
+        if(min!=i)
+        {
+            int temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+        }
+
+    }
+
+    for(int i=0;i<n;i++)
+        printf("%d\n",a[i]);
+    return 0;
+}
 
